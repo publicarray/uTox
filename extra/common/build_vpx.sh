528eeb3dc5cc99694030db309eda32365542af8b
Sebastian Schmidt <publicarray@users.noreply.github.com>
2018-08-09 21:36:27 +1000

    update travis

--- extra/common/build_vpx.sh
+++ extra/common/build_vpx.sh
@@ -9,13 +9,14 @@ fi
 cd libvpx
 git rev-parse HEAD > libvpx.sha
 if ! ([ -f "${CACHE_DIR}/libvpx.sha" ] && diff "${CACHE_DIR}/libvpx.sha" libvpx.sha); then
-  ./configure "$TARGET_TRGT" \
+  ./configure ${TARGET_TRGT} \
               --prefix="${CACHE_DIR}/usr" \
               --enable-static \
+              --enable-pic \
               --disable-examples \
               --disable-unit-tests \
               --disable-shared
-  make -j8
+  make -j`sysctl -n hw.ncpu`
   make install
   mv libvpx.sha "${CACHE_DIR}/libvpx.sha"
 fi
