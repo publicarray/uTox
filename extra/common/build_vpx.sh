97f0725e751811177373d3169e7609eed681f4a2
Sebastian Schmidt <publicarray@users.noreply.github.com>
2018-08-09 21:30:48 +1000

    Update libvpx
    Update travis files

--- extra/common/build_vpx.sh
+++ extra/common/build_vpx.sh
@@ -1,19 +1,22 @@
 #/usr/bin/env zsh
 
+LIBVPX_VERSION="1.7.0"
+
 # install libvpx, needed for video encoding/decoding
 if ! [ -d libvpx ]; then
-  git clone --depth=1 --branch=v1.6.0 https://chromium.googlesource.com/webm/libvpx
+  git clone --depth=1 --branch=v${LIBVPX_VERSION} https://chromium.googlesource.com/webm/libvpx
 fi
 cd libvpx
 git rev-parse HEAD > libvpx.sha
 if ! ([ -f "${CACHE_DIR}/libvpx.sha" ] && diff "${CACHE_DIR}/libvpx.sha" libvpx.sha); then
-  ./configure "$TARGET_TRGT" \
+  ./configure ${TARGET_TRGT} \
               --prefix="${CACHE_DIR}/usr" \
               --enable-static \
+              --enable-pic \
               --disable-examples \
               --disable-unit-tests \
               --disable-shared
-  make -j8
+  make -j`sysctl -n hw.ncpu`
   make install
   mv libvpx.sha "${CACHE_DIR}/libvpx.sha"
 fi
